69fecb101cf1749c9724545d936d2565eaedb7c5
Petersomond1 <petersomond1@gmail.com>
2024-11-23 19:47:24 -0500

    added server and yaml files

---
+++ Api/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+
+
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the 'build' directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Route all requests to the index.html file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
